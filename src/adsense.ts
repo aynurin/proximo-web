ecf0eee22b9a394849a98b9f43d63dfa18d6ec05
Shavkat Aynurin <shav.ayn@outlook.com>
2020-03-02 21:43:31 -0800

    Adsense retry

---
+++ src/adsense.ts
@@ -0,0 +1,21 @@
+import {customElement, noView} from "aurelia-framework";
+ 
+@noView()
+@customElement('adsense')
+export class AdSense {
+  scripttag: HTMLScriptElement;
+ 
+  attached() {
+    this.scripttag = document.createElement('script');
+    this.scripttag.setAttribute('src', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+    this.scripttag.setAttribute('data-ad-client', 'ca-pub-8635029046616817');
+    this.scripttag.async = true;
+    document.body.appendChild(this.scripttag);
+  }
+ 
+  detached() {
+    if (this.scripttag) {
+      this.scripttag.remove();
+    }
+  }
+}
