318d90d5ff986313566c821eceb4b723b7ddcd46
Shavkat Aynurin <shav.ayn@outlook.com>
2020-05-19 21:42:54 -0700

    Added subarray implementation

---
+++ src/components/subarray.ts
@@ -0,0 +1,75 @@
+
+export function subarray<T>(arr: Array<T>, lowEdge: (item: T) => boolean, highEdge: (item: T) => boolean) {
+  if (arr == null) {
+    return null;
+  }
+  let start = edgeSearch(arr, lowEdge);
+  let end = edgeSearch(arr, highEdge);
+  if (start == -1 || end == -1) {
+    return [];
+  } else {
+    if (start == -2) {
+      start = 0;
+    }
+    if (end == -2) {
+      end = arr.length - 1;
+    }
+  }
+  return arr.slice(start, end + 1);
+}
+
+/**
+ * Searches for a boundary within an array. Accepts a comparer that tells `true` from `false` and returns the index of the `true` closest to a `false`.
+ * The function assumes that there is only one boundary. If there is more than one, the function will return any of the boundaries.
+ * For example:
+ *    arr: [1, 1, 1, 2, 2, 2]
+ *    comparer: x => x == 2
+ * the function will return 3 because arr[3] is the first `true` after the `false` series.
+ * @param arr array to search within
+ * @param comparer a comparer function that tells "black from white"
+ * @param startIndex an inclusive start index limitation if any
+ * @param endIndex an exclusive end index limitation if any
+ */
+export function edgeSearch<T>(arr: Array<T>, comparer: (ob: T) => boolean, startIndex: number = 0, endIndex: number = -1) {
+  if (arr == null) {
+    return -1;
+  }
+  if (endIndex < 0 || endIndex >= arr.length) {
+    endIndex = arr.length;
+  }
+  if (startIndex < 0) {
+    startIndex = 0;
+  }
+  if (startIndex >= arr.length) {
+    return -1;
+  }
+  let zeroCheck = comparer(arr[startIndex]);
+  let maxiCheck = comparer(arr[endIndex - 1]);
+  if (zeroCheck === false && maxiCheck === false) {
+    return -1; // no edge and all is false
+  } else if (zeroCheck === true && maxiCheck === true) {
+    return -2; // no edge and all is true
+  }
+  let step = Math.trunc((endIndex - startIndex) / 10);
+  if (step == 0) {
+    step = 1;
+  }
+  let lastResult = null;
+  for (let i = startIndex; i < endIndex; i += step) {
+    let result = comparer(arr[i]);
+    if (lastResult == null) {
+      lastResult = result;
+    } else if (lastResult != result) {
+      if (step == 1) {
+        if (result === true) {
+          return i;
+        } else {
+          return i - step;
+        }
+      } else {
+        return edgeSearch(arr, comparer, i - step, i);
+      }
+    }
+  }
+  return -1;
+}
---
+++ test/unit/components/subarray.spec.ts
@@ -0,0 +1,177 @@
+import { subarray, edgeSearch } from "../../../src/components/subarray";
+
+describe("SubArray", () => {
+  it("subarray-100: subarray regular case", done => {
+    const given = [1, 2, 2, 3, 3, 4, 4, 5, 6];
+    const lowEdge = i => i > 2;
+    const highEdge = i => i < 5;
+    const expected = [3, 3, 4, 4];
+    const actual = subarray(given, lowEdge, highEdge);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-110: subarray nose on the edge 1", done => {
+    const given = [4, 4, 5, 6];
+    const lowEdge = i => i >= 3;
+    const highEdge = i => i < 5;
+    const expected = [4, 4];
+    const actual = subarray(given, lowEdge, highEdge);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-110: subarray nose on the edge 2", done => {
+    const given = [3, 4, 4, 5, 6];
+    const lowEdge = i => i >= 3;
+    const highEdge = i => i < 5;
+    const expected = [3, 4, 4];
+    const actual = subarray(given, lowEdge, highEdge);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-120: subarray tail on the edge 1", done => {
+    const given = [1, 2, 2, 3, 3];
+    const lowEdge = i => i >= 3;
+    const highEdge = i => i < 4;
+    const expected = [3, 3];
+    const actual = subarray(given, lowEdge, highEdge);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-125: subarray tail on the edge 2", done => {
+    const given = [1, 2, 2, 3, 3, 4];
+    const lowEdge = i => i >= 3;
+    const highEdge = i => i < 4;
+    const expected = [3, 3];
+    const actual = subarray(given, lowEdge, highEdge);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-130: subarray out of bounds", done => {
+    const given = [1, 2, 2, 3, 3];
+    const lowEdge = i => i >= 5;
+    const highEdge = i => i < 10;
+    const expected = [];
+    const actual = subarray(given, lowEdge, highEdge);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-140: subarray catch tail", done => {
+    const given = [1, 2, 2, 3, 3];
+    const lowEdge = i => i >= 3;
+    const highEdge = i => i < 10;
+    const expected = [3, 3];
+    const actual = subarray(given, lowEdge, highEdge);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-200: edgeSearch left", done => {
+    const given = [1, 2, 2, 3, 3, 4, 4, 5, 6];
+    const searcher = i => i < 3;
+    const expected = given.findIndex(v => v == 3) - 1;
+    const actual = edgeSearch(given, searcher);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-210: edgeSearch left with start boundary and no edge", done => {
+    const given = [1, 2, 2, 3, 3, 4, 4, 5, 6];
+    const searcher = i => i < 3;
+    const startFrom = 5;
+    const expected = given.slice(startFrom).findIndex(searcher);
+    const actual = edgeSearch(given, searcher, startFrom);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-220: edgeSearch left with start boundary and no edge but all true", done => {
+    const given = [1, 2, 2, 3, 3, 4, 4, 5, 6];
+    const searcher = i => i > 3;
+    const startFrom = 5;
+    const expected = -2;
+    const actual = edgeSearch(given, searcher, startFrom);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-300: edgeSearch right", done => {
+    const given = [1, 2, 2, 3, 3, 4, 4, 5, 6];
+    const searcher = i => i > 4;
+    const expected = given.findIndex(v => v == 5);
+    const actual = edgeSearch(given, searcher);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-310: edgeSearch right with end boundary and no edge", done => {
+    const given = [1, 2, 2, 3, 3, 4, 4, 5, 6];
+    const searcher = i => i > 4;
+    const endsWith = 5;
+    const expected = given.slice(0, endsWith).findIndex(searcher);
+    const actual = edgeSearch(given, searcher, undefined, endsWith);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-320: edgeSearch right with end boundary and no edge but all true", done => {
+    const given = [1, 2, 2, 3, 3, 4, 4, 5, 6];
+    const searcher = i => i < 4;
+    const endsWith = 5;
+    const expected = -2;
+    const actual = edgeSearch(given, searcher, undefined, endsWith);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-400: edgeSearch all true", done => {
+    const given = [1, 2, 3];
+    const searcher = _ => true;
+    const expected = -2;
+    const actual = edgeSearch(given, searcher);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-410: edgeSearch from doc", done => {
+    const given = [1, 1, 1, 2, 2, 2];
+    const searcher = x => x == 2
+    const expected = given.findIndex(searcher);
+    const actual = edgeSearch(given, searcher);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-500: edgeSearch no edge and all false", done => {
+    const given = [1, 2, 3];
+    const searcher = i => false;
+    const expected = given.findIndex(searcher);
+    const actual = edgeSearch(given, searcher);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-600: edgeSearch foolproof out of bounds", done => {
+    const given = [1, 2, 3];
+    const searcher = i => true;
+    const startFrom = given.length;
+    const expected = given.slice(startFrom).findIndex(searcher);
+    const actual = edgeSearch(given, searcher, startFrom);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-605: edgeSearch foolproof out of bounds", done => {
+    const given = [1, 2, 3];
+    const searcher = _ => true;
+    const startFrom = given.length + 100;
+    const expected = given.slice(startFrom).findIndex(searcher);
+    const actual = edgeSearch(given, searcher, startFrom);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-620: edgeSearch foolproof no edge and all true", done => {
+    const given = [1, 2, 3];
+    const searcher = _ => true;
+    const endsWith = given.length + 100;
+    const expected = -2;
+    const actual = edgeSearch(given, searcher, undefined, endsWith);
+    expect(actual).toEqual(expected);
+    done();
+  });
+  it("subarray-630: edgeSearch foolproof null array", done => {
+    const searcher = i => true;
+    const expected = -1;
+    const actual = edgeSearch(null, searcher);
+    expect(actual).toEqual(expected);
+    done();
+  });
+});
